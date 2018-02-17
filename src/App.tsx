6779ea25df00276900ac2fadf9aa5f1c39f796ed
Glavin Wiechert <glavin.wiechert@gmail.com>
2018-02-17 16:16:29 -0400

    Add JSON Schema form editor for options

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import { Playground } from "./Playground";
 require('./styles/lux.css');
+require('./styles/main.css');
 
 export default class App extends React.Component<any, any> {
     render() {
--- src/Playground.tsx
+++ src/Playground.tsx
@@ -3,17 +3,12 @@ import { Link } from "react-router-dom";
 import Unibeautify, { BeautifyData } from "unibeautify";
 import * as CodeMirror from "react-codemirror";
 import * as _ from "lodash";
+import Form, { FormProps, IChangeEvent } from "react-jsonschema-form";
 
 require("codemirror/lib/codemirror.css");
 require("codemirror/mode/javascript/javascript");
 
 import beautify from "./beautify";
-import beautifiers from "./beautifiers";
-Unibeautify.loadBeautifiers(beautifiers);
-
-(window as any).unibeautify = Unibeautify;
-
-const languageName = "JavaScript";
 
 export class Playground extends React.Component<
   PlaygroundProps,
@@ -22,33 +17,29 @@ export class Playground extends React.Component<
   constructor(props: any) {
     super(props);
     this.state = {
-      languageName: "Javascript",
+      languageName: "JavaScript",
       originalText: `function helloWorld() {
 console.log('Hello World');
 }`,
       beautifiedText: "",
-      options: JSON.stringify(
-        {
-          JavaScript: {
-            beautifiers: ["Pretty Diff", "Prettier"],
-            align_assignments: false,
-            arrow_parens: "as-needed",
-            break_chained_methods: true,
-            end_with_comma: true,
-            end_with_semicolon: true,
-            indent_char: " ",
-            indent_size: 2,
-            jsx_brackets: false,
-            multiline_ternary: true,
-            object_curly_spacing: true,
-            quotes: "double",
-            space_after_anon_function: false,
-            wrap_line_length: 80
-          } as any
-        },
-        null,
-        2
-      )
+      options: {
+        JavaScript: {
+          beautifiers: ["Pretty Diff", "Prettier"],
+          align_assignments: false,
+          arrow_parens: "as-needed",
+          break_chained_methods: true,
+          end_with_comma: true,
+          end_with_semicolon: true,
+          indent_char: " ",
+          indent_size: 2,
+          jsx_brackets: false,
+          multiline_ternary: true,
+          object_curly_spacing: true,
+          quotes: "double",
+          space_after_anon_function: false,
+          wrap_line_length: 80
+        } as any
+      }
     };
     this.beautify = _.throttle(this.beautify.bind(this), 1000, {
       trailing: true
@@ -57,23 +48,23 @@ console.log('Hello World');
   }
 
   public render() {
-    const { originalText, beautifiedText, options } = this.state;
+    const log = (type: string) => console.log.bind(console, type);
+    const { languageName, originalText, beautifiedText, options } = this.state;
     return (
       <div className="container-fluid">
         <div className="jumbotron">
           <h1 className="display-3 text-center">Unibeautify Playground</h1>
           <div className="row">
-            <div className="col-sm">
-              <CodeMirror
-                value={options}
+            <div className="col-sm-2">
+              <Form
+                schema={this.schemaForLanguage(languageName)}
+                formData={this.langOptions(languageName)}
                 onChange={this.onChangeOptions}
-                options={{
-                  lineNumbers: true,
-                  mode: "javascript"
-                }}
+                onSubmit={log("submitted")}
+                onError={log("errors")}
               />
             </div>
-            <div className="col-sm">
+            <div className="col-sm-5">
               <CodeMirror
                 value={originalText}
                 onChange={this.onChangeText}
@@ -83,7 +74,7 @@ console.log('Hello World');
                 }}
               />
             </div>
-            <div className="col-sm">
+            <div className="col-sm-5">
               <CodeMirror
                 key={beautifiedText}
                 value={beautifiedText}
@@ -99,10 +90,15 @@ console.log('Hello World');
     );
   }
 
-  private onChangeOptions = (newValue: string): void => {
+  private onChangeOptions = (changeEvent: IChangeEvent): void => {
+    const { languageName } = this.state;
+    const { formData } = changeEvent;
     this.setState(prevState => ({
       ...prevState,
-      options: newValue
+      options: {
+        ...prevState.options,
+        [languageName]: formData
+      }
     }));
     this.beautify();
   };
@@ -126,18 +122,109 @@ console.log('Hello World');
 
   private get beautifyPayload(): BeautifyData {
     return {
-      languageName: languageName,
+      languageName: this.state.languageName,
       options: this.beautifyOptions,
       text: this.state.originalText
     };
   }
 
   private get beautifyOptions(): BeautifyData["options"] {
-    try {
-      return JSON.parse(this.state.options);
-    } catch (error) {
-      return {};
-    }
+    return this.state.options || {};
+  }
+
+  private langOptions(languageName: string): BeautifyData["options"][string] {
+    return this.beautifyOptions[languageName] || {};
+  }
+
+  private schemaForLanguage(languageName: string): FormProps["schema"] {
+    return this.schema;
+  }
+
+  private get schema(): FormProps["schema"] {
+    return {
+      title: `${this.state.languageName} Options`,
+      type: "object",
+      //   required: ["title"],
+      properties: {
+        beautifiers: {
+          type: "array",
+          title: "Beautifiers",
+          items: {
+            type: "string",
+            default: "Prettier",
+            enum: ["Prettier", "Pretty Diff"]
+          }
+        },
+        align_assignments: {
+          type: "boolean",
+          title: "Align Assignments",
+          default: false
+        },
+        arrow_parens: {
+          type: "string",
+          title: "Arrow Parens",
+          default: "as-needed",
+          enum: ["as-needed", "always"]
+        },
+        break_chained_methods: {
+          type: "boolean",
+          title: "Break Chained Methods",
+          default: true
+        },
+        end_with_comma: {
+          type: "boolean",
+          title: "End with comma",
+          default: true
+        },
+        end_with_semicolon: {
+          type: "boolean",
+          title: "End with semicolon",
+          default: true
+        },
+        indent_char: {
+          type: "string",
+          title: "Indent Char",
+          default: " ",
+          enum: [" ", "\t"]
+        },
+        indent_size: {
+          type: "integer",
+          title: "Indent Size",
+          default: 2
+        },
+        jsx_brackets: {
+          type: "boolean",
+          title: "JSX Brackets",
+          default: false
+        },
+        multiline_ternary: {
+          type: "boolean",
+          title: "Multiline Ternary",
+          default: true
+        },
+        object_curly_spacing: {
+          type: "boolean",
+          title: "Object curly spacing",
+          default: true
+        },
+        quotes: {
+          type: "string",
+          title: "Quotes",
+          default: "double",
+          enum: ["double", "single"]
+        },
+        space_after_anon_function: {
+          type: "boolean",
+          title: "Space after anonymous function",
+          default: false
+        },
+        wrap_line_length: {
+          type: "integer",
+          title: "Wrap line length",
+          default: 80
+        }
+      }
+    };
   }
 }
 
@@ -145,7 +232,9 @@ interface PlaygroundProps {}
 
 interface PlaygroundState {
   languageName: string;
-  options: string; // BeautifyData["options"]
+  options: {
+    [languageName: string]: BeautifyData["options"];
+  };
   originalText: string;
   beautifiedText: string;
 }
