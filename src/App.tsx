7fbcecdcaffa4eeb66cd29a5949a8f3d6189b9fd
Steven Zeck <steven.zeck@gmail.com>
2018-02-22 23:26:49 -0600

    Use icons for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "./Container";
+require("font-awesome/css/font-awesome.css");
 require("./styles/main.css");
 
 export default class App extends React.Component<any, any> {
