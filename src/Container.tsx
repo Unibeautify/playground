e12acaa1088e948093c97203f71bcd87f311f784
Glavin Wiechert <glavin.wiechert@gmail.com>
2020-08-04 19:42:36 -0300

    Update API to use Prod URL

--- src/Container.tsx
+++ src/Container.tsx
@@ -19,7 +19,7 @@ import { ReportIssueButton } from "./ReportIssueButton";
 import { Nav } from "./Nav";
 
 const apiUrl: string =
-  "https://ntd6xp2n56.execute-api.us-east-1.amazonaws.com/dev/playground";
+  "https://z446o1y0w8.execute-api.us-east-1.amazonaws.com/prod/playground";
 
 (window as any).Unibeautify = Unibeautify;
 
